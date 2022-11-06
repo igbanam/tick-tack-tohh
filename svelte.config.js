508f6263877a5e6eba7de8796a49614c742afba8
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-11-06 21:51:59 +0000

    Deploy for Github Pages (#5)
    * yarn add -D #sveltejs/adapter-static
    
    * Configure adapter-static
    
    This builds locally
    
    * yarn add gh-pages
    
    * Add the "deploy" script
    
    * Add "Deployment" action
    
    Everything from this point on would he handled on _release_

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: false
+    })
   }
 };
 
