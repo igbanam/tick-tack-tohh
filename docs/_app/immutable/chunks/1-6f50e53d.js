fc3847a6995eaa614ef639302e5f924ed1315ee2
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-11-06 23:15:15 +0000

    Revert to adapter-auto; prep for Cloudflare

--- docs/_app/immutable/chunks/1-6f50e53d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-9c613878.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'docs',
-      assets: 'docs',
-      fallback: null,
-      precompress: false,
-      strict: false
-    })
+    adapter: adapter()
   }
 };
 
