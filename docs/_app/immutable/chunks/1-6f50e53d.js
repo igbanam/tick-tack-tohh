61703d85658236e75d17d5d73fbf4cb929c666b9
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-11-06 22:58:57 +0000

    build into docs

---
+++ docs/_app/immutable/chunks/1-6f50e53d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-9c613878.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
+      pages: 'docs',
+      assets: 'docs',
       fallback: null,
       precompress: false,
       strict: false
