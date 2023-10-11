dd9f61fad4aa91ca67734b6d9de308e703ab1d8e
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2023-10-11 11:59:13 +0000

    Conditional adapter on environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import { default as local_adapter } from '@sveltejs/adapter-auto';
+import { default as prod_adapter } from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: import.meta.env.DEV ? local_adapter() : prod_adapter()
   }
 };
 
